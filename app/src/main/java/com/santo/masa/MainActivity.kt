3222a24d5a5e31480ec7d56c141bc778ef66fec5
santo <jorge.arangoa@udea.edu.co>
2023-09-10 23:14:55 -0500

    punto 3

---
+++ app/src/main/java/com/santo/masa/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.santo.masa
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.santo.masa.databinding.ActivityMainBinding
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var mainbinding: ActivityMainBinding
+    private var fechaNacimiento: String = ""
+    private var cal= Calendar.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainbinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainbinding.root
+        setContentView(view)
+
+        val dataSetListener =
+            DatePickerDialog.OnDateSetListener { view, year: Int, month, DayOfMonth ->
+                cal.set(Calendar.YEAR,year)
+                cal.set(Calendar.MONTH,month)
+                cal.set(Calendar.DAY_OF_MONTH,DayOfMonth)
+
+                val  format = " MM/dd/yyyy"
+                val sdf = SimpleDateFormat(format,Locale.US)
+                fechaNacimiento = sdf.format(cal.time).toString()
+                mainbinding.dateButton6.setText(fechaNacimiento)
+            }
+        mainbinding.dateButton6.setOnClickListener{
+            DatePickerDialog(
+                this,
+                dataSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH),
+
+
+                ).show()
+
+        }
+
+
+
+
+        val df = DecimalFormat("#.#")
+
+        mainbinding.calcularButton.setOnClickListener {
+            if (!validateEmpty()){
+                val peso = mainbinding.pesoInputEditex.text.toString().toDouble()
+                val altura = mainbinding.alturaInputEditex.text.toString().toDouble()
+                val imc = peso/ altura.pow(2)
+                mainbinding.resultadoTextView2.text = buildString {
+                    append(" su imc ")
+                    append(df.format(imc))
+                }
+                if (imc< 18.5) mainbinding.recomendacionTextView3.text = ("bien")
+                if (imc>=18.5 && imc<24.9) mainbinding.recomendacionTextView3.text = ("buen peso")
+                if (imc>=24.9 && imc<29.9) mainbinding.recomendacionTextView3.text = ("sobrepeso")
+                if (imc>=29.9) mainbinding.recomendacionTextView3.text = ("obeso")
+            } else {
+                Toast.makeText(this, "digite bien", Toast.LENGTH_SHORT).show()
+
+
+
+
+
+            }
+
+
+
+        }
+
+
+    }
+    private fun validateEmpty() =
+        mainbinding.alturaInputEditex.text.toString().isEmpty() || mainbinding.pesoInputEditex.text.toString()
+            .isEmpty()
+}
